1d7d691e5c60bd3087b1c1a0dd52635dd9c1802a
kkommus <keiti.k6mmus@gmail.com>
2024-02-18 19:24:04 +0200

    uuendus

--- kalkulaatori_tootlemise_skript.php
+++ kalkulaatori_tootlemise_skript.php
@@ -1,15 +1,22 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["arvuta"])) {
-    $hind = 0;
+    // Kontrollime, kas teenuste valik on tehtud
+    if ($_POST["algajatele_koolitus"] == "none" && $_POST["edasijoudnutele_koolitus"] == "none") {
+        echo "<p class='text-danger'>Palun vali vähemalt üks koolitus!</p>";
+    } else {
+        // Siin võid jätkata oma kalkulatsiooni loogikaga
+        // Näiteks, kui hind on alati 199€ või 299€ vastavalt valikule
+        $hind = 0;
 
-    if ($_POST["algajatele_koolitus"] != "none") {
-        $hind += 199;
+        if ($_POST["algajatele_koolitus"] != "none") {
+            $hind += 199;
+        }
+
+        if ($_POST["edasijoudnutele_koolitus"] != "none") {
+            $hind += 299;
+        }
+
+        echo "<p class='text-success'>Kokku maksumus: " . $hind . "€</p>";
     }
-
-    if ($_POST["edasijoudnutele_koolitus"] != "none") {
-        $hind += 299;
-    }
-
-    echo "Kokku maksumus: " . $hind . "€";
 }
 ?>
